73dc35940814e26d4b0fad42fc4ffe5f31e246de
idu135 <marsha85@mail.ru>
2016-05-25 15:22:13 +0300

    25/05

---
+++ site/js/script.js
@@ -0,0 +1,8 @@
+(function(document, window, $){
+  $(document).ready(function(){
+    $(".slider").slick({
+      prevArrow: '.controls .prev',
+      nextArrow: '.controls .next',
+    });
+  });
+})(document, window, jQuery);
